d0715dc64e53475788a3733684f9c32000cee276
Ozapata91 <orlando@staylisted.com>
2025-07-24 19:23:54 -0700

    Create google_auth.js

---
+++ google_auth.js
@@ -0,0 +1,48 @@
+import fs from "fs";
+import readline from "readline";
+import { google } from "googleapis";
+
+const SCOPES = [
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive.file",
+  "https://www.googleapis.com/auth/gmail.send"
+];
+const TOKEN_PATH = "token.json";
+
+export async function authorizeGoogle() {
+  const credentials = JSON.parse(fs.readFileSync("credentials.json"));
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+  if (fs.existsSync(TOKEN_PATH)) {
+    const token = JSON.parse(fs.readFileSync(TOKEN_PATH));
+    oAuth2Client.setCredentials(token);
+    return oAuth2Client;
+  }
+
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  console.log("👉 Authorize this app by visiting this URL:", authUrl);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const code = await new Promise((resolve) => {
+    rl.question("Enter the code from that page here: ", (code) => {
+      rl.close();
+      resolve(code);
+    });
+  });
+
+  const { tokens } = await oAuth2Client.getToken(code);
+  oAuth2Client.setCredentials(tokens);
+  fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
+  console.log("✅ Token stored to", TOKEN_PATH);
+  return oAuth2Client;
+}
